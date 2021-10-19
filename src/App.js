a56830e63857b30f3952a1d5a06259bbf07481b6
federicodonner_oracle <federico.donner@oracle.com>
2021-10-19 08:19:21 -0500

    feat(App): scroll to the prompt when contentStack is updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import ConsoleTx from "./ConsoleTx";
 import Tx from "./Tx";
@@ -10,6 +10,15 @@ export default function App() {
     { type: "consoleTx", text: "welcome" },
   ]);
 
+  // Whenever something is added to the content stack, the page
+  // scrolls to the bottom to emulate the behaviour of terminals
+  useEffect(() => {
+    // Doesn't work without the setTimeout for some reason...
+    setTimeout(() => {
+      window.scrollTo(0, document.body.scrollHeight);
+    });
+  }, [contentStack]);
+
   // Function invoked by the Prompt component
   // Performs an action depending on the type returned by Prompt
   function pushContentStack(newContent) {
@@ -17,6 +26,16 @@ export default function App() {
     if (newContent.type === "clear") {
       setContentStack([{ type: "consoleTx", text: "welcome" }]);
       return;
+    }
+    // If the new prompt is a download type, trigger the download
+    // then write the message
+    if (newContent.type === "download") {
+      console.log(newContent.file);
+      newContent = {
+        type: "consoleTx",
+        text: "downloading",
+        command: newContent.command,
+      };
     }
     // Copies the arry in state
     var auxiliaryArray = JSON.parse(JSON.stringify(contentStack));
